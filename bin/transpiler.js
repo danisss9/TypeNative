a1ba5f6b1b22fdf5849e96f1b270b350755c3052
Dani Santos <daniramossantos9@gmail.com>
2024-03-01 00:51:01 +0000

    added support for vars and binary expressions

--- bin/transpiler.js
+++ bin/transpiler.js
@@ -1,12 +1,13 @@
 import ts from 'typescript';
+let TypeCheker;
 export function transpileToC(code) {
     const sourceFile = ts.createSourceFile('main.ts', code, ts.ScriptTarget.ES2020, true, ts.ScriptKind.TS);
+    TypeCheker = ts.createProgram(['main.ts'], {}).getTypeChecker();
     return `#include <stdio.h>
 #include <string.h>
 
 int main() {
-    ${visit(sourceFile)}
-    return 0;
+    ${visit(sourceFile)}return 0;
 }
     `;
 }
@@ -18,18 +19,58 @@ export function visit(node) {
     else if (ts.isStringLiteral(node)) {
         return `"${node.text}"`;
     }
+    else if (ts.isNumericLiteral(node)) {
+        return `${node.text}`;
+    }
+    else if (ts.isToken(node) && node.kind === ts.SyntaxKind.TrueKeyword) {
+        return `1`;
+    }
+    else if (ts.isToken(node) && node.kind === ts.SyntaxKind.FalseKeyword) {
+        return `0`;
+    }
     else if (ts.isPropertyAccessExpression(node)) {
         return `${visit(node.expression)}.${visit(node.name)}`;
+    }
+    else if (ts.isVariableDeclaration(node)) {
+        const type = getCType(node.type);
+        const initializer = node.initializer ? ` = ${visit(node.initializer)}` : '';
+        return `${type.typeStr} ${visit(node.name)}${type.arrayStr}${initializer}`;
     }
     else if (ts.isCallExpression(node)) {
         const expr = visit(node.expression);
         const leftSide = expr === 'console.log' ? 'printf' : expr;
         return `${leftSide}(${node.arguments.map((a) => visit(a)).join(',')})`;
     }
-    else if (ts.isExpressionStatement(node)) {
-        return visit(node.expression) + ';\n';
+    else if (ts.isBinaryExpression(node)) {
+        return `${visit(node.left)} ${node.operatorToken.getText()} ${visit(node.right)}`;
     }
-    // console.log(ts.SyntaxKind[node.kind], node.getText());
-    ts.forEachChild(node, (subNode) => (code = code + visit(subNode)));
+    else if (ts.isParenthesizedExpression(node)) {
+        return `(${visit(node.expression)})`;
+    }
+    else if (ts.isVariableDeclarationList(node)) {
+        return node.declarations.map((n) => visit(n)).join(';\n\t') + ';\n\t';
+    }
+    else if (ts.isExpressionStatement(node)) {
+        return visit(node.expression) + ';\n\t';
+    }
+    console.log(ts.SyntaxKind[node.kind], node.getText());
+    ts.forEachChild(node, (subNode) => {
+        code += visit(subNode);
+        return null;
+    });
     return code;
+}
+function getCType(typeNode) {
+    const type = TypeCheker.getTypeFromTypeNode(typeNode);
+    const typeName = TypeCheker.typeToString(type);
+    switch (typeName) {
+        case 'string':
+            return { typeStr: 'char', arrayStr: '[]' };
+        case 'number':
+            return { typeStr: 'double', arrayStr: '' };
+        case 'boolean':
+            return { typeStr: 'int', arrayStr: '' };
+        default:
+            return { typeStr: typeName, arrayStr: '' };
+    }
 }
--- src/transpiler.ts
+++ src/transpiler.ts
@@ -1,5 +1,7 @@
 import ts from 'typescript';
 
+let TypeCheker: ts.TypeChecker;
+
 export function transpileToC(code: string): string {
   const sourceFile = ts.createSourceFile(
     'main.ts',
@@ -9,12 +11,13 @@ export function transpileToC(code: string): string {
     ts.ScriptKind.TS
   );
 
+  TypeCheker = ts.createProgram(['main.ts'], {}).getTypeChecker();
+
   return `#include <stdio.h>
 #include <string.h>
 
 int main() {
-    ${visit(sourceFile)}
-    return 0;
+    ${visit(sourceFile)}return 0;
 }
     `;
 }
@@ -26,18 +29,52 @@ export function visit(node: ts.Node): string {
     return node.text;
   } else if (ts.isStringLiteral(node)) {
     return `"${node.text}"`;
+  } else if (ts.isNumericLiteral(node)) {
+    return `${node.text}`;
+  } else if (ts.isToken(node) && node.kind === ts.SyntaxKind.TrueKeyword) {
+    return `1`;
+  } else if (ts.isToken(node) && node.kind === ts.SyntaxKind.FalseKeyword) {
+    return `0`;
   } else if (ts.isPropertyAccessExpression(node)) {
     return `${visit(node.expression)}.${visit(node.name)}`;
+  } else if (ts.isVariableDeclaration(node)) {
+    const type = getCType(node.type!);
+    const initializer = node.initializer ? ` = ${visit(node.initializer)}` : '';
+    return `${type.typeStr} ${visit(node.name)}${type.arrayStr}${initializer}`;
   } else if (ts.isCallExpression(node)) {
     const expr = visit(node.expression);
     const leftSide = expr === 'console.log' ? 'printf' : expr;
     return `${leftSide}(${node.arguments.map((a) => visit(a)).join(',')})`;
+  } else if (ts.isBinaryExpression(node)) {
+    return `${visit(node.left)} ${node.operatorToken.getText()} ${visit(node.right)}`;
+  } else if (ts.isParenthesizedExpression(node)) {
+    return `(${visit(node.expression)})`;
+  } else if (ts.isVariableDeclarationList(node)) {
+    return node.declarations.map((n) => visit(n)).join(';\n\t') + ';\n\t';
   } else if (ts.isExpressionStatement(node)) {
-    return visit(node.expression) + ';\n';
+    return visit(node.expression) + ';\n\t';
   }
 
-  // console.log(ts.SyntaxKind[node.kind], node.getText());
-  ts.forEachChild(node, (subNode) => (code = code + visit(subNode)));
+  console.log(ts.SyntaxKind[node.kind], node.getText());
+  ts.forEachChild(node, (subNode) => {
+    code += visit(subNode);
+    return null;
+  });
 
   return code;
+}
+
+function getCType(typeNode: ts.TypeNode): { typeStr: string; arrayStr: string } {
+  const type = TypeCheker.getTypeFromTypeNode(typeNode);
+  const typeName = TypeCheker.typeToString(type);
+  switch (typeName) {
+    case 'string':
+      return { typeStr: 'char', arrayStr: '[]' };
+    case 'number':
+      return { typeStr: 'double', arrayStr: '' };
+    case 'boolean':
+      return { typeStr: 'int', arrayStr: '' };
+    default:
+      return { typeStr: typeName, arrayStr: '' };
+  }
 }
